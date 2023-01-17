b96b5fcaf23cb2695d0a4be15abf830bbdd8fa23
ezechielemsallem <ezeemsallem@gmail.com>
2023-01-17 10:18:19 +0200

    first commit

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from . import views
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('product',views.APIViews.as_view()),
+    path('login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register', views.register),
+
+]
+
+
